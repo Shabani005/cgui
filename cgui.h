4167deb87be366defd65d09c0d5847aa0fdbf2d0
shabani005 <hasan.shabani2005@gmail.com>
2025-07-07 00:07:12 +0300

    Feat: Implemeted button but did not test

--- cgui.h
+++ cgui.h
@@ -1,1 +1,13 @@
 #include "raylib.h"
+#include <cstdio>
+
+void DrawButton(int posX, int posY, int width, int height, Color color){
+  DrawRectangle(posX, posY, width, height, color);
+  int mouseX = GetMouseX();
+  int mouseY = GetMouseY();
+  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+    if (mouseX >= width-posX && mouseX <= width+posX && mouseY >= height-posY && mouseY <= height+posY){
+      printf("hello world");
+    }
+  }
+}
